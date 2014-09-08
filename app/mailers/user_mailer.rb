8cb3f539754affd226d786f919ab7f77711af75e
ml242 <mattlucas@gmail.com>
2014-09-08 17:16:46 -0400

    fixes email issue in sendgrid

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ActionMailer::Base
-  default from: "marisa@sweetroots.com",
+  default from: "marisa@sweetrootsnyc.com",
           :bcc => "mattlucas@gmail.com"
 
 
   def welcome_mailer(user)
     @user = user
     attachments["burrito_sm.jpg"] = File.read("#{Rails.root}/public/images/burrito_sm.jpg")
-    mail(:to => user.email, :subject => "Welcome you rocking roller!")
+    mail(:to => user.email, :subject => "Welcome, you rocking roller!")
   end
 
   # def admin_mailer(admin)
