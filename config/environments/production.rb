7c8c0356fd15e822f85ccc772a520bc24f2cdc76
ml242 <mattlucas@gmail.com>
2014-09-08 16:58:12 -0400

    adds sendgrid to generate emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,11 @@ Rails.application.configure do
     config.action_mailer.delivery_method = :smtp
     # config.action_mailer.raise_delivery_errors = true
     config.action_mailer.smtp_settings = {
-    :host => '/',
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => 'gmail.com',
-    :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_PASSWORD"],
+    :user_name            => ENV["SENDGRID_USERNAME"],
+    :password             => ENV["SENDGRID_PASSWORD"],
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
 
