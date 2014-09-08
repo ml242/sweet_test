df7ce551b0afc30f05cab1965021738e5fae007e
ml242 <mattlucas@gmail.com>
2014-09-08 17:05:55 -0400

    toggling static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-
+  config.serve_static_assets = true
+  
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.raise_delivery_errors = false
